4984303d23e1e134bbb08ee390ea6626b2f2b9d7
axolotle <nchesnais@mailoo.org>
2022-05-27 14:48:41 +0200

    temp disable news pagination

--- script/script.js
+++ script/script.js
@@ -43,7 +43,7 @@ showMenu(menu);
 
 window.addEventListener('resize', () => {
     showMenu(menu);
-    paginationSwitch()
+    // paginationSwitch()
 });
 
 
@@ -58,47 +58,47 @@ const gallery = document.getElementById('gallery');
 const figureWidth = gallery.firstElementChild.offsetWidth + 40;
 
 /* To fix */
-paginationButtons.forEach((button) => {
-   button.onclick = () => {
-       // Next button
-       if (button == next) {
+// paginationButtons.forEach((button) => {
+//    button.onclick = () => {
+//        // Next button
+//        if (button == next) {
+//
+//            let nextLeftSpace = gallery.scrollLeftMax - (gallery.scrollLeft + figureWidth);
+//            if (nextLeftSpace < figureWidth) {
+//                gallery.scrollBy(gallery.scrollLeftMax, 0);
+//                next.disabled = true;
+//            }
+//
+//            else {
+//                // Scroll left
+//                gallery.scrollBy(figureWidth, 0);
+//            }
+//            // Active prev button if left scroll
+//            if (gallery.scrollLeft > 0) {
+//                prev.disabled = false;
+//            }
+//        }
+//        // Prev button
+//        else {
+//            let nextLeftSpace = gallery.scrollLeft - figureWidth;
+//            if (nextLeftSpace < figureWidth) {
+//                // Scroll right
+//                gallery.scrollTo(0, 0)
+//                prev.disabled = true;
+//            }
+//            else {
+//               gallery.scrollBy(-figureWidth, 0);
+//            }
+//            // Active next button if content to scroll
+//            if (gallery.scrollLeft < gallery.scrollLeftMax) {
+//                next.disabled = false;
+//            }
+//
+//        }
+//    }
+// });
 
-           let nextLeftSpace = gallery.scrollLeftMax - (gallery.scrollLeft + figureWidth);
-           if (nextLeftSpace < figureWidth) {
-               gallery.scrollBy(gallery.scrollLeftMax, 0);
-               next.disabled = true;
-           }
-
-           else {
-               // Scroll left
-               gallery.scrollBy(figureWidth, 0);
-           }
-           // Active prev button if left scroll
-           if (gallery.scrollLeft > 0) {
-               prev.disabled = false;
-           }
-       }
-       // Prev button
-       else {
-           let nextLeftSpace = gallery.scrollLeft - figureWidth;
-           if (nextLeftSpace < figureWidth) {
-               // Scroll right
-               gallery.scrollTo(0, 0)
-               prev.disabled = true;
-           }
-           else {
-              gallery.scrollBy(-figureWidth, 0);
-           }
-           // Active next button if content to scroll
-           if (gallery.scrollLeft < gallery.scrollLeftMax) {
-               next.disabled = false;
-           }
-
-       }
-   }
-});
-
-paginationSwitch()
+// paginationSwitch()
 
 
 
